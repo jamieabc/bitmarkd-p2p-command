5bc3f69bf6b2bed58e8341d5e7af7db5eb4ad402
Aaron Chang <xesuschang@gmail.com>
2019-08-13 18:13:48 +0800

    change ip from yaml

--- main.rb
+++ main.rb
@@ -4,7 +4,9 @@ require "ffi-rzmq"
 require "json"
 require "yaml"
 
-address = "127.0.0.1:2136"
+config_file = "config.yml"
+
+yml_config = YAML.load_file(config_file)
 
 def open_zmq_sock
   ctx = ZMQ::Context.new
@@ -20,7 +22,7 @@ end
 
 def socket_encryption(socket)
   socket.setsockopt(ZMQ::CURVE_SERVER, 0)
-  socket.setsockopt(ZMQ::CURVE_SERVERKEY, ["e7387aceec876838bf16c26d6bc2a491dfd8b36983f4046d35ef0ab509427f51"].pack("H*").to_s)
+  socket.setsockopt(ZMQ::CURVE_SERVERKEY, ["683054c7d2a9eca754fb7e20c3c73ed5abeaaccf075b2a5fd7dda9bb49003171"].pack("H*").to_s)
   socket.setsockopt(ZMQ::CURVE_PUBLICKEY, ["4ef2be6a85f606efc5a137b9e79c09b5798c7d6c8e6136be35fcce946c704d38"].pack("H*").to_s)
   socket.setsockopt(ZMQ::CURVE_SECRETKEY, ["7e596e1d5b6563487fef18ab5a8eef1fdd48f3cad3e55d3e4c958df59f43008c"].pack("H*").to_s)
 end
@@ -43,14 +45,32 @@ end
 def open_sock(address)
   s = open_zmq_sock
   socket_option(s)
-  s.connect("tcp://#{address}")
+  s.connect(address)
   s
 end
 
-s = open_sock(address)
+def connection(ip, port)
+  "tcp://#{ip}:#{port}"
+end
 
-s.send_string ("local", ZMQ::SNDMORE)
-s.send_string ("I", 0)
-msgs = []
-s.recv_strings(msgs)
-puts "msg: #{msgs}"
+sockets = []
+
+yml_config["nodes"].each do |n|
+  sockets << open_sock(connection(n["ip4"], n["port"]))
+end
+
+def parse_response(resp)
+  hsh = JSON.parse(resp).to_h
+  puts "response: #{hsh}"
+end
+
+sockets.each do |s|
+  s.send_string("testing", ZMQ::SNDMORE)
+  s.send_string("I", 0)
+  msgs = []
+  s.recv_strings(msgs)
+  parse_response(msgs[1])
+end
+
+sockets.each(&:close)
+
