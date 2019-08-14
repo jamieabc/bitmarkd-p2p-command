81050971ec2783f99375f0ca1c05e97e0e7f01ee
Aaron Chang <xesuschang@gmail.com>
2019-08-14 14:11:12 +0800

    add yaml config for all nodes
    Squashed commit of the following:
    
    commit 107bcb9109b913c7fa25b391b589df674e76f435
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Aug 14 14:02:11 2019 +0800
    
        use name for message
    
    commit 711b18d7f3ee207cdfaf7eabbcd77dec6e0a8eef
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Aug 14 13:58:58 2019 +0800
    
        put related methods into class
    
    commit bb56bd1f6f4dad74446b220220913db35d7a3e94
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Aug 13 18:24:15 2019 +0800
    
        add yaml config sample
    
    commit 7a906f242955b8a7d78c20b8648f07ff1a6e5cbd
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Aug 13 18:17:19 2019 +0800
    
        refactor, use yaml for node config
    
    commit 5bc3f69bf6b2bed58e8341d5e7af7db5eb4ad402
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Aug 13 18:13:48 2019 +0800
    
        change ip from yaml

--- main.rb
+++ main.rb
@@ -3,54 +3,32 @@
 require "ffi-rzmq"
 require "json"
 require "yaml"
+require_relative 'socket'
 
-address = "127.0.0.1:2136"
+config_file = "config.yml"
 
-def open_zmq_sock
-  ctx = ZMQ::Context.new
-  ctx.socket(ZMQ::REQ)
+yml_config = YAML.load_file(config_file)
+
+Socket.set_keys(yml_config["public_key"], yml_config["private_key"])
+
+sockets = []
+
+puts "create zmq sockets"
+yml_config["nodes"].each do |n|
+  sockets << Socket.new(ip: n["ip4"], port: n["port"], remote_public_key: n["public_key"], name: n["name"])
 end
 
-def socket_tcp_keepalive(socket)
-  socket.setsockopt(ZMQ::TCP_KEEPALIVE, 1)
-  socket.setsockopt(ZMQ::TCP_KEEPALIVE_CNT, 5)
-  socket.setsockopt(ZMQ::TCP_KEEPALIVE_IDLE, 60)
-  socket.setsockopt(ZMQ::TCP_KEEPALIVE_INTVL, 60)
+def parse_response(name, resp)
+  hsh = JSON.parse(resp).to_h
+  puts "#{name} response: #{hsh}"
 end
 
-def socket_encryption(socket)
-  socket.setsockopt(ZMQ::CURVE_SERVER, 0)
-  socket.setsockopt(ZMQ::CURVE_SERVERKEY, ["e7387aceec876838bf16c26d6bc2a491dfd8b36983f4046d35ef0ab509427f51"].pack("H*").to_s)
-  socket.setsockopt(ZMQ::CURVE_PUBLICKEY, ["4ef2be6a85f606efc5a137b9e79c09b5798c7d6c8e6136be35fcce946c704d38"].pack("H*").to_s)
-  socket.setsockopt(ZMQ::CURVE_SECRETKEY, ["7e596e1d5b6563487fef18ab5a8eef1fdd48f3cad3e55d3e4c958df59f43008c"].pack("H*").to_s)
+puts "query bitmarkd info"
+sockets.each do |s|
+  s.send("testing", ZMQ::SNDMORE)
+  s.send("I", 0)
+  msgs = s.receive
+  parse_response(s.name, msgs[1])
 end
 
-def random_identity
-  (0..31).map { rand(69..91).chr }.join
-end
-
-def socket_option(zmq_socket)
-  socket_encryption(zmq_socket)
-  zmq_socket.setsockopt(ZMQ::IMMEDIATE, 1)
-  zmq_socket.setsockopt(ZMQ::IDENTITY, random_identity)
-  zmq_socket.setsockopt(ZMQ::REQ_CORRELATE, 1)
-  zmq_socket.setsockopt(ZMQ::REQ_RELAXED, 1)
-
-  socket_tcp_keepalive(zmq_socket)
-  zmq_socket.setsockopt(ZMQ::MAXMSGSIZE, 5_000_000)
-end
-
-def open_sock(address)
-  s = open_zmq_sock
-  socket_option(s)
-  s.connect("tcp://#{address}")
-  s
-end
-
-s = open_sock(address)
-
-s.send_string ("local", ZMQ::SNDMORE)
-s.send_string ("I", 0)
-msgs = []
-s.recv_strings(msgs)
-puts "msg: #{msgs}"
+sockets.each(&:close)
