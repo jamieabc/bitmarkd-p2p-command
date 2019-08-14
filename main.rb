711b18d7f3ee207cdfaf7eabbcd77dec6e0a8eef
Aaron Chang <xesuschang@gmail.com>
2019-08-14 13:58:58 +0800

    put related methods into class

--- main.rb
+++ main.rb
@@ -3,60 +3,19 @@
 require "ffi-rzmq"
 require "json"
 require "yaml"
+require_relative 'socket'
 
 config_file = "config.yml"
 
 yml_config = YAML.load_file(config_file)
 
-def open_zmq_sock
-  ctx = ZMQ::Context.new
-  ctx.socket(ZMQ::REQ)
-end
-
-def socket_tcp_keepalive(socket)
-  socket.setsockopt(ZMQ::TCP_KEEPALIVE, 1)
-  socket.setsockopt(ZMQ::TCP_KEEPALIVE_CNT, 5)
-  socket.setsockopt(ZMQ::TCP_KEEPALIVE_IDLE, 60)
-  socket.setsockopt(ZMQ::TCP_KEEPALIVE_INTVL, 60)
-end
-
-def socket_encryption(socket, server_public_key, client_public_key, client_private_key)
-  socket.setsockopt(ZMQ::CURVE_SERVER, 0)
-  socket.setsockopt(ZMQ::CURVE_SERVERKEY, [server_public_key].pack("H*").to_s)
-  socket.setsockopt(ZMQ::CURVE_PUBLICKEY, [client_public_key].pack("H*").to_s)
-  socket.setsockopt(ZMQ::CURVE_SECRETKEY, [client_private_key].pack("H*").to_s)
-end
-
-def random_identity
-  (0..31).map { rand(69..91).chr }.join
-end
-
-def socket_option(zmq_socket, server_public_key, client_public_key, client_private_key)
-  socket_encryption(zmq_socket, server_public_key, client_public_key, client_private_key)
-  zmq_socket.setsockopt(ZMQ::IMMEDIATE, 1)
-  zmq_socket.setsockopt(ZMQ::IDENTITY, random_identity)
-  zmq_socket.setsockopt(ZMQ::REQ_CORRELATE, 1)
-  zmq_socket.setsockopt(ZMQ::REQ_RELAXED, 1)
-
-  socket_tcp_keepalive(zmq_socket)
-  zmq_socket.setsockopt(ZMQ::MAXMSGSIZE, 5_000_000)
-end
-
-def open_sock(address, server_public_key, client_public_key, client_private_key)
-  s = open_zmq_sock
-  socket_option(s, server_public_key, client_public_key, client_private_key)
-  s.connect(address)
-  s
-end
-
-def connection(ip, port)
-  "tcp://#{ip}:#{port}"
-end
+Socket.set_keys(yml_config["public_key"], yml_config["private_key"])
 
 sockets = []
 
+puts "create zmq sockets"
 yml_config["nodes"].each do |n|
-  sockets << open_sock(connection(n["ip4"], n["port"]), n["public_key"], yml_config["public_key"], yml_config["private_key"])
+  sockets << Socket.new(n["ip4"], n["port"], n["public_key"])
 end
 
 def parse_response(resp)
@@ -64,13 +23,12 @@ def parse_response(resp)
   puts "response: #{hsh}"
 end
 
+puts "query bitmarkd info"
 sockets.each do |s|
-  s.send_string("testing", ZMQ::SNDMORE)
-  s.send_string("I", 0)
-  msgs = []
-  s.recv_strings(msgs)
+  s.send("testing", ZMQ::SNDMORE)
+  s.send("I", 0)
+  msgs = s.receive
   parse_response(msgs[1])
 end
 
 sockets.each(&:close)
-
---
+++ socket.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+# Socket is for abstraction of zeromq socket
+class Socket
+  attr_reader :remote_public_key, :ip, :port, :socket
+
+  @@public_key = ""
+  @@private_key = ""
+
+  def initialize(ip, port, remote_public_key)
+    set_connection(ip, port)
+    @remote_public_key = remote_public_key
+    @socket = create_zmq_socket
+    socket_option
+    socket.connect(connection)
+  end
+
+  def connection
+    "tcp://#{ip}:#{port}"
+  end
+
+  def send(str, zmq_flag)
+    socket.send_string(str, zmq_flag)
+  end
+
+  def receive
+    messages = []
+    socket.recv_strings(messages)
+    messages
+  end
+
+  def close
+    socket.close
+  end
+
+  def self.set_keys(public, private)
+    @@public_key = public
+    @@private_key = private
+  end
+
+  private
+
+  def create_zmq_socket
+    ctx = ZMQ::Context.new
+    ctx.socket(ZMQ::REQ)
+  end
+
+  def set_connection(ip, port)
+    @ip = ip
+    @port = port
+  end
+
+  def random_identity
+    (0..31).map { rand(69..91).chr }.join
+  end
+
+  def socket_option
+    set_socket_encryption
+    socket.setsockopt(ZMQ::IMMEDIATE, 1)
+    socket.setsockopt(ZMQ::IDENTITY, random_identity)
+    socket.setsockopt(ZMQ::REQ_CORRELATE, 1)
+    socket.setsockopt(ZMQ::REQ_RELAXED, 1)
+    socket_tcp_keepalive
+    socket.setsockopt(ZMQ::MAXMSGSIZE, 5_000_000)
+  end
+
+  def set_socket_encryption
+    socket.setsockopt(ZMQ::CURVE_SERVER, 0)
+    socket.setsockopt(ZMQ::CURVE_SERVERKEY, [remote_public_key].pack("H*").to_s)
+    socket.setsockopt(ZMQ::CURVE_PUBLICKEY, [@@public_key].pack("H*").to_s)
+    socket.setsockopt(ZMQ::CURVE_SECRETKEY, [@@private_key].pack("H*").to_s)
+  end
+
+  def socket_tcp_keepalive
+    socket.setsockopt(ZMQ::TCP_KEEPALIVE, 1)
+    socket.setsockopt(ZMQ::TCP_KEEPALIVE_CNT, 5)
+    socket.setsockopt(ZMQ::TCP_KEEPALIVE_IDLE, 60)
+    socket.setsockopt(ZMQ::TCP_KEEPALIVE_INTVL, 60)
+  end
+end
